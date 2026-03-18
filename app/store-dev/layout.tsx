e5ed1014704f2fa84e99e33679c6c4447b4e0229
Nildaproduction <nildapublishing@gmail.com>
2026-03-18 02:05:16 +0100

    Create layout.tsx

---
+++ app/store-dev/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css"
+import { Playfair_Display, Poppins } from "next/font/google"
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  variable: "--font-heading",
+})
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500"],
+  variable: "--font-body",
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={`${playfair.variable} ${poppins.variable}`}>
+      <body>{children}</body>
+    </html>
+  )
+}
