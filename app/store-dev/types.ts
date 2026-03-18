c29927ccc7ada3785f795af59a07f078d416bce2
Nildaproduction <nildapublishing@gmail.com>
2026-03-18 04:34:30 +0100

    Update types.ts

--- app/store-dev/types.ts
+++ app/store-dev/types.ts
@@ -1,9 +1,11 @@
+// /app/store-dev/types.ts
 export type Product = {
   id: string
   name: string
   price: number
   category: "Music" | "TShirt" | "Merch"
   images: string[]
+  description: string
+  type: "digital" | "physical"
   stock: number
-}
 }
