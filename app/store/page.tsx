f7e21d4a12a67b3b7f7f71d09964c22ee7fcb429
Nildaproduction <nildapublishing@gmail.com>
2026-03-23 07:01:48 +0100

    Rename Home to StorePage and add iframe

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,5 +1,13 @@
-import { redirect } from "next/navigation";
+'use client'
 
-export default function Home() {
-  redirect("/store");
+export default function StorePage() {
+  return (
+    <div className="w-full min-h-screen">
+      <iframe
+        src="https://tikizikike.vercel.app"
+        className="w-full h-screen border-none"
+        title="TIKIZIKI Store"
+      />
+    </div>
+  )
 }
