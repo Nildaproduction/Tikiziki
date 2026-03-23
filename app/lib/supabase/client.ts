1b6fde6b927d5a3a968bbcc24f15f6449fecd66a
Nildaproduction <nildapublishing@gmail.com>
2026-03-23 17:54:43 +0100

    Refactor Supabase client creation logic
    Refactor Supabase client initialization and remove redirect.

--- app/lib/supabase/client.ts
+++ app/lib/supabase/client.ts
@@ -1,5 +1,19 @@
-import { redirect } from "next/navigation";
+'use client'
 
-export default function Home() {
-  redirect("/store");
+import { createBrowserClient } from '@supabase/ssr'
+
+export function createClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    if (process.env.NODE_ENV === 'development') {
+      console.warn('Supabase URL and Anon Key must be set in environment variables')
+      return null
+    } else {
+      throw new Error('Supabase URL and Anon Key must be set in environment variables')
+    }
+  }
+
+  return createBrowserClient(supabaseUrl, supabaseAnonKey)
 }
