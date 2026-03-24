9873e7a97d99bbde83b1b802c8a78f022f3968a3
Nildaproduction <nildapublishing@gmail.com>
2026-03-24 03:48:57 +0100

    Refactor StoreWrapper to StorePage component

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,31 +1,13 @@
-'use client';
+'use client'
 
-import { useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
-
-export default function StoreWrapper() {
-  const searchParams = useSearchParams();
-  const productId = searchParams.get('product');
-
-  const storeBaseUrl = 'https://tikizikike.vercel.app';
-
-  useEffect(() => {
-    const iframe = document.getElementById('store-iframe') as HTMLIFrameElement;
-    if (iframe) {
-      iframe.src = productId
-        ? `${storeBaseUrl}/store/product/${productId}`
-        : `${storeBaseUrl}/store`;
-    }
-  }, [productId]);
-
+export default function StorePage() {
   return (
-    <div className="w-full min-h-screen flex flex-col">
+    <div className="w-full min-h-screen">
       <iframe
-        id="store-iframe"
-        src={storeBaseUrl + '/store'}
-        className="flex-1 w-full h-full border-none"
+        src="https://tikizikike.vercel.app"
+        className="w-full h-screen border-none"
         title="TIKIZIKI Store"
       />
     </div>
-  );
+  )
 }
